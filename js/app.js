53a1241e0954bff101a3a640a58905d15a234a89
sheylabre <sheylabrenaicha@hotmail.com>
2016-12-02 11:52:11 -0500

    doble click

--- js/app.js
+++ js/app.js
@@ -1,68 +1,55 @@
 ;
 (function() {
     window.addEventListener("load", function() {
-        var fruta = document.getElementById("drag1");
+        var manzana = document.getElementById("manzana");
+        var platano = document.getElementById("platano")
+        var naranja = document.getElementById("naranja")
+        var mandarina = document.getElementById("mandarina")
+        var fresa = document.getElementById("fresa")
         var dia = document.getElementsByClassName("alimento");
         var seccion = document.getElementById("menu-de-frutas");
         var contador = 0;
-
         for (var i = 0; i < dia.length; i++) {
-            fruta.addEventListener("dragstart", function(evento) {
+            manzana.addEventListener("dragstart", function(evento) {
                 inicio(evento, this);
             });
-
             dia[i].addEventListener("drop", function(evento) {
                 clonar(evento, this);
-
             });
-
             dia[i].addEventListener("dragover", function(ev) {
                 ev.preventDefault();
-
             });
-        }
-
-        for (var i = 0; i < dia.length; i++) {
-            document.getElementById("drag0").addEventListener("dragstart", function(evento) {
+            platano.addEventListener("dragstart", function(evento) {
+                inicio(evento, this);
+            });
+            naranja.addEventListener("dragstart", function(evento) {
+                inicio(evento, this);
+            });
+            mandarina.addEventListener("dragstart", function(evento) {
+                inicio(evento, this);
+            });
+            fresa.addEventListener("dragstart", function(evento) {
                 inicio(evento, this);
             });
         }
-
         function inicio(e) {
-            e.dataTransfer.setData("img", e.target.id); //Coje el elemento que se va a mover
+            e.dataTransfer.setData("img", e.target.id); 
         }
 
         function clonar(e) {
-            var elementoArrastrado = document.getElementById(e.dataTransfer.getData("img")); // Elemento arrastrado
-            elementoArrastrado.style.opacity = ''; // Dejamos la opacidad a su estado anterior para copiar el elemento igual que era antes
-            var elementoClonado = elementoArrastrado.cloneNode(true); // Se clona el elemento
-            elementoClonado.id = "ElemClonado" + contador; // Se cambia el id porque tiene que ser unico
+            var elementoArrastrado = document.getElementById(e.dataTransfer.getData("img")); 
+            elementoArrastrado.style.opacity = ''; 
+            var elementoClonado = elementoArrastrado.cloneNode(true); 
+            elementoClonado.id = "ElemClonado" + contador;
             contador += 1;
-            elementoClonado.style.position = "static"; // Se posiciona de forma "normal" (Sino habria que cambiar las coordenadas de la posición)
-            e.target.appendChild(elementoClonado); // Se añade el elemento clonado
-            e.target.style.border = ''; // Quita el borde del "cuadro clonador"
+            elementoClonado.style.position = "static"; 
+            e.target.appendChild(elementoClonado); 
+            e.target.style.border = ''; 
+            	elementoClonado.addEventListener("dblclick",function(){
+            		elementoClonado.style.display = "none";
+            	});
         }
-
-
     });
-
-
-
-
 })()
 
-/*
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
-*/
+    
